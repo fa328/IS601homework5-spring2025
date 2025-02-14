073dd2d99cd963bc86ab1cd40e469d65fff6baa3
fa328 <fa328@njit.edu>
2025-02-14 18:53:05 -0500

    update Calculator init Master

---
+++ calculator/__init__.py
@@ -0,0 +1,12 @@
+'''Calculator Level 1 Master'''
+def add(a,b):
+    return a + b
+
+def subtract(a,b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
