5a3a84b91b44a969ad6f5209cdb44ea6d3e6e959
fa328 <fa328@njit.edu>
2025-02-14 19:00:17 -0500

    Update calculator init master

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,12 +1,16 @@
 '''Calculator Level 1 Master'''
 def add(a,b):
+    '''Test that addition function works '''    
     return a + b
 
 def subtract(a,b):
+    '''Test that subtract function works '''    
     return a - b
 
 def multiply(a, b):
+    '''Test that multiply function works '''    
     return a * b
 
 def divide(a, b):
+    '''Test that divide function works '''    
     return a / b
