4edb0751e5f43cabb6d52dc52e999a9a8f7889d3
fa328 <fa328@njit.edu>
2025-02-14 19:24:20 -0500

    Update calculator operations part 3

---
+++ calculator/operations.py
@@ -0,0 +1,13 @@
+'''Test Calculation: Part 3'''
+def add(a,b):
+    '''Test that addition function works '''    
+    return a + b
+def subtract(a,b):
+    '''Test that subract function works '''    
+    return a - b
+def multiply(a,b):
+    '''Test that multiply function works '''    
+    return a * b
+def divide(a,b):
+    '''Test that divide function works '''    
+    return a / b
