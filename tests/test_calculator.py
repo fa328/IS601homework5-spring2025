68af3a645ec4f93605ce67489d955537a1f7363a
fa328 <fa328@njit.edu>
2025-02-14 19:40:14 -0500

    Update test calculator Part 3

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,24 @@
 '''My Calculator Test: Part 3'''
-from calculator import add, subtract, multiply, divide
+from calculator import add, subtract, multiply, divide, Calculator, CalculationsHistory
 
 def test_addition():
     '''Test that addition function works '''    
-    assert add(2,2) == 4
+    assert add(2, 2) == 4
 
 def test_subtraction():
     '''Test that subtract function works '''    
-    assert subtract(2,2) == 0
+    assert subtract(2, 2) == 0
 
 def test_multiplication():
-    '''Test that multiply fuction works'''
-    assert multiply(2,2) == 4
+    '''Test that multiply function works'''
+    assert multiply(2, 2) == 4
 
 def test_division():
-    '''Test that division fuction works'''
-    assert divide(2,2) == 1
+    '''Test that division function works'''
+    assert divide(2, 2) == 1
+
+# Calculator and CalculationsHistory
+calc = Calculator()
+result = calc.add(2, 2)
+CalculationsHistory.add_history(result)
+print(CalculationsHistory.get_history())
