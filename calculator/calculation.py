eb5ceb2711b4b821321aa2535ec5a6ba184b1013
fa328 <fa328@njit.edu>
2025-02-14 19:23:39 -0500

    Update calculator file Part 3

---
+++ calculator/calculation.py
@@ -0,0 +1,19 @@
+'''Calculations Class functions: Part 3'''
+class Calculations:  # pylint: disable=too-few-public-methods
+    '''Calculations functions'''
+
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation = operation
+
+    def get_result(self):
+        '''Get result of the operation'''
+        return self.operation(self.a, self.b)
+
+def add(a, b):
+    '''Add function'''
+    return a + b
+
+calc = Calculations(2, 2, add)
+print(calc.get_result())
