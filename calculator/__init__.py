33fe4965b82c90ed52adb48d54b520a022d71be1
fa328 <fa328@njit.edu>
2025-02-14 19:25:35 -0500

    Update calculator init part 3

---
+++ calculator/__init__.py
@@ -0,0 +1,56 @@
+'''My Calculator Test: Part 3'''
+import pytest # type: ignore
+from calculator.calculation import Calculations
+from calculator.operations import add, subtract, multiply, divide
+
+class Calculator:
+    '''Calculations functions'''
+
+    @staticmethod
+    def add(a: float, b: float) -> float:
+        '''Test that addition function works '''    
+        calculation = Calculations(a, b, add)
+        return calculation.get_result()
+
+    @staticmethod
+    def subtract(a: float, b: float) -> float:
+        '''Test that subtract function works '''    
+        calculation = Calculations(a, b, subtract)
+        return calculation.get_result()
+
+    @staticmethod
+    def multiply(a: float, b: float) -> float:
+        '''Test that multiply function works '''    
+        calculation = Calculations(a, b, multiply)
+        return calculation.get_result()
+
+    @staticmethod
+    def divide(a: float, b: float) -> float:
+        '''Test that divide function works '''    
+        if b == 0:
+            return "Error: Division by zero"
+        return Calculations(a, b, divide).get_result()
+
+class CalculationsHistory:
+    '''To store history'''
+    history = []
+
+    @classmethod
+    def add_history(cls, calculation: float):
+        '''Performs addition, subtraction, multiplication, division'''
+        cls.history.append(calculation)
+
+    @classmethod
+    def get_history(cls):
+        '''To get the history of calculations'''
+        return cls.history
+
+    @classmethod
+    def clear_history(cls):
+        '''Use to clears the history'''
+        cls.history = []
+
+calc = Calculator()
+result = calc.add(2, 2)
+CalculationsHistory.add_history(result)
+print(CalculationsHistory.get_history())
